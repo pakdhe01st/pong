41528cf22c0670c1c26d3a4e3ad8e2f3cdd05b11
M. Fahreza Alfa Sina Mustofa <44043655+pakdhe01st@users.noreply.github.com>
2020-03-20 15:00:05 +0700

    Add files via upload

---
+++ ballscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballscript : MonoBehaviour
+{
+    public int speed=30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Heloo World!");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1,-1)*speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
